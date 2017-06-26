f776dd7f790831abe34df4ea73652f1b4bd812ac
Colin Verot <colin@verot.net>
2017-06-26 04:43:05 +0200

    updated Swedish translation

--- src/lang/class.upload.sv_SE.php
+++ src/lang/class.upload.sv_SE.php
@@ -39,7 +39,7 @@
  */
 
     $translation = array();
-    $translation['file_error']                  = 'Fil fel. Försök igen.';
+    $translation['file_error']                  = 'Filfel. Försök igen.';
     $translation['local_file_missing']          = 'Lokal fil hittades inte.';
     $translation['local_file_not_readable']     = 'Gick inte att skriva till lokal fil.';
     $translation['uploaded_too_big_ini']        = 'Uppladdningsfel (Den uppladdade filen överskrider upload_max_filesize direktivet i php.ini).';
@@ -48,37 +48,37 @@
     $translation['uploaded_missing']            = 'Uppladdningsfel (Ingen fil blev uppladdad).';
     $translation['uploaded_unknown']            = 'Uppladdningsfel (Okänt fel).';
     $translation['try_again']                   = 'Uppladdningsfel. Försök igen.';
-    $translation['file_too_big']                = 'Filen är förstor.';
-    $translation['no_mime']                     = 'MIME typen kan inte hittas.';
-    $translation['incorrect_file']              = 'Inkorrekt fil typ.';
+    $translation['file_too_big']                = 'Filen är för stor.';
+    $translation['no_mime']                     = 'MIME-typen kan inte hittas.';
+    $translation['incorrect_file']              = 'Inkorrekt filtyp.';
     $translation['image_too_wide']              = 'Bilden är för bred.';
     $translation['image_too_narrow']            = 'Bilden är för smal.';
-    $translation['image_too_high']              = 'Bilden är för hög.';
-    $translation['image_too_short']             = 'Bilden är för liten.';
-    $translation['ratio_too_high']              = 'Bild förhållandet är för stort (Bilden är för bred).';
-    $translation['ratio_too_low']               = 'Bild förhållandet är för litet (Bilden är för hög).';
+    $translation['image_too_high']              = 'Bildens höjd är för hög.';
+    $translation['image_too_short']             = 'Bildens höjd är för låg.';
+    $translation['ratio_too_high']              = 'Bildförhållandet är för stort (Bilden är för bred).';
+    $translation['ratio_too_low']               = 'Bildförhållandet är för litet (Bilden är för hög).';
     $translation['too_many_pixels']             = 'Bilden har för många pixlar.';
     $translation['not_enough_pixels']           = 'Bilden har inte tillräckligt med pixlar.';
-    $translation['file_not_uploaded']           = 'Bilden är inte uppladdad kan inte fortsätta förloppet.';
+    $translation['file_not_uploaded']           = 'Bilden är inte uppladdad. Processen har stoppats.';
     $translation['already_exists']              = '%s finns redan. Ändra filnamnet.';
-    $translation['temp_file_missing']           = 'Fel temporär kälfil. Kan inte fortsätta förloppet.';
-    $translation['source_missing']              = 'Fel uppladdad temporär kälfil. Kan inte fortsätta förloppet.';
-    $translation['destination_dir']             = 'Mål katalogen kan inte skapas. Kan inte fortsätta förloppet.';
-    $translation['destination_dir_missing']     = 'Mål katalogen finns inte. Kan inte fortsätta förloppet.';
-    $translation['destination_dir_write']       = 'Mål katalogen kan inte göras skrivbar. Kan inte fortsätta förloppet.';
-    $translation['destination_path_write']      = 'Mål katalogen är inte skrivbar. Kan inte fortsätta förloppet.';
-    $translation['temp_file']                   = 'Kan inte skapa den temporära filen. Kan inte fortsätta förloppet.';
-    $translation['source_not_readable']         = 'Käll filen är inte skrivbar. Kan inte fortsätta förloppet.';
+    $translation['temp_file_missing']           = 'Fel temporär källfil. Processen har stoppats.';
+    $translation['source_missing']              = 'Fel uppladdad temporär kälfil. Processen har stoppats.';
+    $translation['destination_dir']             = 'Målkatalogen kan inte skapas. Processen har stoppats.';
+    $translation['destination_dir_missing']     = 'Målkatalogen finns inte. Processen har stoppats.';
+    $translation['destination_dir_write']       = 'Målkatalogen kan inte göras skrivbar. Processen har stoppats.';
+    $translation['destination_path_write']      = 'Målkatalogen är inte skrivbar. Processen har stoppats.';
+    $translation['temp_file']                   = 'Kan inte skapa den temporära filen. Processen har stoppats.';
+    $translation['source_not_readable']         = 'Källfilen är inte skrivbar. Processen har stoppats.';
     $translation['no_create_support']           = 'Inget stöd för skapandet av %s.';
-    $translation['create_error']                = 'Fel vid skapandet av %s bilden.';
-    $translation['source_invalid']              = 'Kan inte läsa bilden. Är det en bild?.';
+    $translation['create_error']                = 'Fel vid skapandet av %s.';
+    $translation['source_invalid']              = 'Kan inte läsa in bilden. Är det en bild?';
     $translation['gd_missing']                  = 'GD Biblioteket verkar inte vara installerat på servern.';
-    $translation['watermark_no_create_support'] = 'Inget stöd för skapandet av %s, Kan inte läsa vattenstämpeln.';
-    $translation['watermark_create_error']      = 'Inget stöd för läsandet av %s, kan inte skapa vattenstämplen.';
-    $translation['watermark_invalid']           = 'Okänt bild format, kan inte läsa vattenstämplen.';
+    $translation['watermark_no_create_support'] = 'Inget stöd för skapandet av %s, kan inte läsa vattenstämpel.';
+    $translation['watermark_create_error']      = 'Inget stöd för läsandet av %s, kan inte skapa vattenstämpel.';
+    $translation['watermark_invalid']           = 'Okänt bildformat, kan inte läsa vattenstämpel.';
     $translation['file_create']                 = 'Inget stöd för skapandet av %s.';
-    $translation['no_conversion_type']          = 'Ingen förvandlings typ bestämd.';
-    $translation['copy_failed']                 = 'Fel vid kopieringen utav filen. copy() misslyckades.';
-    $translation['reading_failed']              = 'Fel vid inläsningen utav filen.';   
-        
+    $translation['no_conversion_type']          = 'Ingen omvandlingstyp bestämd.';
+    $translation['copy_failed']                 = 'Fel vid kopiering av filen. copy() misslyckades.';
+    $translation['reading_failed']              = 'Fel vid inläsning av filen.';
+
 ?>
